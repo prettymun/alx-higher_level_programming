5ae74fbb5bf695c220f488fd0ea298aa77793c78
Purity musambi <avulamusipurity@gmail.com>
2024-03-19 12:38:30 -0700

    python strcts

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for column in row:
-            if column == row[-1]:
-                print('{:d}'.format(column), end='')
-            else:
-                print('{:d}'.format(column), end=' ')
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j != 0:
+                print(" ", end='')
+                print("{:d}".format(matrix[i][j]), end='')
                 print()
